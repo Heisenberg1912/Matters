3fdba2ede129c373677a6ff29baae5407cbd9396
tushar batham <batham.tushar2001@gmail.com>
2025-12-25 14:25:01 +0530

    new changes

--- test-login.ps1
+++ test-login.ps1
@@ -3,5 +3,14 @@ $body = @{
     password = 'customer123'
 } | ConvertTo-Json
 
-$response = Invoke-RestMethod -Uri 'http://localhost:4000/api/session/login' -Method Post -Body $body -ContentType 'application/json'
-$response | ConvertTo-Json -Depth 10
+try {
+    $response = Invoke-RestMethod -Uri 'http://localhost:4000/api/session/login' -Method Post -Body $body -ContentType 'application/json'
+    Write-Host "Success!" -ForegroundColor Green
+    $response | ConvertTo-Json -Depth 2
+} catch {
+    Write-Host "Error:" -ForegroundColor Red
+    $_.Exception.Message
+    if ($_.ErrorDetails.Message) {
+        $_.ErrorDetails.Message
+    }
+}
